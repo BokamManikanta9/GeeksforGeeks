4a6cbf51551c0aafdd2ecdf204c2942199531e8a
BokamManikanta9 <119719201+BokamManikanta9@users.noreply.github.com>
2025-03-11 11:02:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Set kth bit/set-kth-bit.py
+++ Difficulty: Basic/Set kth bit/set-kth-bit.py
@@ -1,27 +1,22 @@
 #User function Template for python3
 class Solution:
-	def setKthBit(self, n, K):
-	    mask=1<<K
-	    n=n|mask
-	    return n
+	def setKthBit(self, n, k):
 		# code here
-
+		x=1<<k
+		return n|x
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		N,K = input().split()
-		N = int(N)
-		K = int(K)
-		ob = Solution()
-		ans = ob.setKthBit(N,K)
-		print(ans)
-
-
-
+    T = int(input())
+    for i in range(T):
+        N, K = input().split()
+        N = int(N)
+        K = int(K)
+        ob = Solution()
+        ans = ob.setKthBit(N, K)
+        print(ans)
 
 # } Driver Code Ends
